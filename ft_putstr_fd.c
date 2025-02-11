71cfde9eeba0fd053d746d396d2c21099f232a48
Seán Belometti <148435833+sbelomet@users.noreply.github.com>
2025-02-11 14:13:47 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -14,14 +14,10 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
+	if (!s)
+		return ;
+	while (*s)
+		write(fd, s++, 1);
 }
 
 /*
