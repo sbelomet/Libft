1b9bf07739a1a792eb9b76be7ae0b0357f154587
Seán Belometti <148435833+sbelomet@users.noreply.github.com>
2025-02-11 14:03:25 +0100

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -14,13 +14,7 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
-	write(fd, "\n", 1);
+	while (*s)
+		write(fd, s++, 1);
+	write (fd, "\n", 1);
 }
