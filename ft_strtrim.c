3469d573bcebc4e9c596f2c12f5f7bb9370c2f9b
Seán Belometti <148435833+sbelomet@users.noreply.github.com>
2025-02-11 15:16:06 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,51 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbelomet <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: sbelomet <sbelomet@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/12 11:51:34 by sbelomet          #+#    #+#             */
-/*   Updated: 2023/10/17 11:49:50 by sbelomet         ###   ########.fr       */
+/*   Created: 2023/10/24 16:08:29 by lgosselk          #+#    #+#             */
+/*   Updated: 2025/02/11 15:14:36 by sbelomet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isintable(char const *set, char c)
+char	*ft_strtrim(const char *s1, const char *set)
 {
-	int	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*res;
-	int		start;
+	int		i;
 	int		end;
+	int		start;
+	char	*result;
 
-	end = ft_strlen(s1);
-	while (end > 0 && ft_isintable(set, s1[end - 1]))
-		end--;
 	start = 0;
-	while (ft_isintable(set, s1[start]))
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	if (start >= end)
-	{
-		start = 0;
-		end = 0;
-	}
-	res = (char *)malloc((end - start) * sizeof(char) + 1);
-	if (!res)
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	result = (char *) malloc((end - start + 1) * sizeof(char));
+	if (result == NULL)
 		return (NULL);
-	ft_strlcpy(res, (s1 + start), (end - start + 1));
-	return (res);
+	i = 0;
+	while (start < end)
+		result[i++] = s1[start++];
+	result[i] = '\0';
+	return (result);
 }
 
 /*
