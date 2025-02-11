65142a82586b8d9e412da8d5bbf5abd3a798db66
Seán Belometti <148435833+sbelomet@users.noreply.github.com>
2025-02-11 14:22:16 +0100

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbelomet <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: sbelomet <sbelomet@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:52:28 by sbelomet          #+#    #+#             */
-/*   Updated: 2023/10/10 16:22:18 by sbelomet         ###   ########.fr       */
+/*   Updated: 2025/02/11 14:21:53 by sbelomet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 	int	len;
 
+	if (!s)
+		return (NULL);
 	i = 0;
 	len = ft_strlen(s);
 	while (i < len + 1)
@@ -28,7 +30,7 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-/*
+/* 
 int	main(void)
 {
 	char	str[] = "huntlow & lumity";
@@ -37,8 +39,9 @@ int	main(void)
 	int		to_find3 = 'z';
 	int		to_find4 = 'y';
 
+	printf("str : %p\n", str);
 	printf("to_find1 : %p\n", ft_strchr(str, to_find1));
 	printf("to_find2 : %p\n", ft_strchr(str, to_find2));
 	printf("to_find3 : %p\n", ft_strchr(str, to_find3));
 	printf("to_find4 : %p\n", ft_strchr(str, to_find4));
-}*/
+} */
