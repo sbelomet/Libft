ada8582b2fcb92fdd5fc2e00797098f5009ef74e
Seán Belometti <148435833+sbelomet@users.noreply.github.com>
2025-02-11 14:54:51 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,35 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbelomet <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: sbelomet <sbelomet@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/12 10:58:03 by sbelomet          #+#    #+#             */
-/*   Updated: 2023/10/12 11:17:20 by sbelomet         ###   ########.fr       */
+/*   Created: 2023/10/24 15:38:39 by lgosselk          #+#    #+#             */
+/*   Updated: 2025/02/11 14:54:29 by sbelomet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
 	int		len_s1;
-	int		tlen;
 	int		i;
 	char	*res;
 
 	len_s1 = ft_strlen(s1);
-	tlen = len_s1 + ft_strlen(s2);
-	res = (char *)malloc(tlen * sizeof(char) + 1);
+	res = (char *)malloc((len_s1 + ft_strlen(s2) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		res[i] = s1[i];
 		i++;
 	}
 	i = 0;
-	while (s2[i])
+	while (s2 && s2[i])
 	{
 		res[len_s1 + i] = s2[i];
 		i++;
@@ -40,8 +38,40 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (res);
 }
 
-/*
+char	*ft_strjoin_alloc(char const *s1, char const *s2,
+	t_alloc **alloc)
+{
+	int		len_s1;
+	int		i;
+	char	*res;
+
+	len_s1 = ft_strlen(s1);
+	res = (char *)ft_malloc((len_s1 + ft_strlen(s2) + 1) * sizeof(char), alloc);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2 && s2[i])
+	{
+		res[len_s1 + i] = s2[i];
+		i++;
+	}
+	res[len_s1 + i] = '\0';
+	return (res);
+}
+
+/* 
 int	main(void)
 {
 	printf("result: |%s|\n", ft_strjoin("simon ", "petrikov"));
-}*/
+	printf("result: |%s|\n", ft_strjoin("Finn ", ""));
+	printf("result: |%s|\n", ft_strjoin("", "the Dog"));
+	printf("result: |%s|\n", ft_strjoin("Princess ", NULL));
+	printf("result: |%s|\n", ft_strjoin(NULL, "the Vampire Queen"));
+	printf("result: |%s|\n", ft_strjoin("Ice ", "King"));
+} */
